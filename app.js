4fe755c1816e28a5d777de1df959805e47629d79
Swojas Bhalunkar <swojas12@gmail.com>
2023-07-29 12:12:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -114,6 +114,10 @@ app.get("/blog-posts/:post", function (req, res) {
 
 });
 
+app.use((req, res) => {
+  res.status(404).render("404", {query: ""});
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
